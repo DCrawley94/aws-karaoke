ff0fdffa0fa3f2008d560505ff626bbec2a3d8d3
DCrawley94 <duncan.crawley@gmail.com>
2022-11-29 16:29:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AWS Polly Does Karaoke
+AKA the worst thing I've ever made.
+
+Usage:
+
+1. Awsume into Whizlabs
+2. Clone repo and cd into it
+3. Run the following in the terminal `./aws-karaoke.sh` and answer the questions.
+4. Enjoy your free concert.
---
+++ aws-karaoke.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+songs=$(ls songs)
+singers="Aditi Amy Astrid Bianca Brian Camila Carla Carmen Celine Chantal Conchita Cristiano Dora Emma Enrique Ewa Filiz Gabrielle Geraint Giorgio Gwyneth Hans Ines Ivy Jacek Jan Joanna Joey Justin Karl Kendra Kevin Kimberly Lea Liv Lotte Lucia Lupe Mads Maja Marlene Mathieu Matthew Maxim Mia Miguel Mizuki Naja Nicole Olivia Penelope Raveena Ricardo Ruben Russell Salli Seoyeon Takumi Tatyana Vicki Vitoria Zeina Zhiyu Aria Ayanda Arlet Hannah Arthur Daniel Liam Pedro Kajal Hiujin Laura Elin Ida Suvi Ola Hala"
+
+echo -e "\nChoose your singer:\n"
+
+select name in $songs; do
+    SONG_CHOICE=$name
+    break
+done
+
+echo -e "\n"
+
+select singer in $singers; do
+    SINGER_CHOICE=$singer
+    break
+done
+
+echo -e "You have chosen $SONG_CHOICE performed by by $SINGER_CHOICE\n"
+
+if [ -d "mp3s" ]; then
+    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >/dev/null 2>&1
+else
+    mkdir mp3s
+    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >/dev/null 2>&1
+fi
+
+sleep 1
+
+afplay "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3"
