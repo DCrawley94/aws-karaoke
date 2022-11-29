b957ee93d3561d73de672826488e201adba919da
DCrawley94 <duncan.crawley@gmail.com>
2022-11-29 16:43:41 +0000

    improve text formatting slightly

--- aws-karaoke.sh
+++ aws-karaoke.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-songs=$(ls songs)
+songs=$(ls -1 songs | sed 's/\.[^.]*$//')
 singers="Aditi Amy Astrid Bianca Brian Camila Carla Carmen Celine Chantal Conchita Cristiano Dora Emma Enrique Ewa Filiz Gabrielle Geraint Giorgio Gwyneth Hans Ines Ivy Jacek Jan Joanna Joey Justin Karl Kendra Kevin Kimberly Lea Liv Lotte Lucia Lupe Mads Maja Marlene Mathieu Matthew Maxim Mia Miguel Mizuki Naja Nicole Olivia Penelope Raveena Ricardo Ruben Russell Salli Seoyeon Takumi Tatyana Vicki Vitoria Zeina Zhiyu Aria Ayanda Arlet Hannah Arthur Daniel Liam Pedro Kajal Hiujin Laura Elin Ida Suvi Ola Hala"
 
 echo -e "\nChoose your singer:\n"
@@ -20,10 +20,10 @@ done
 echo -e "You have chosen $SONG_CHOICE performed by by $SINGER_CHOICE\n"
 
 if [ -d "mp3s" ]; then
-    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3"
+    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3"
 else
     mkdir mp3s
-    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3"
+    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3"
 fi
 
 sleep 1
