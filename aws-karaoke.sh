b9599d67b96cbac9a88dd0d6a022d4efbbf1f28c
DCrawley94 <duncan.crawley@gmail.com>
2022-11-30 08:33:43 +0000

    refactor script

--- aws-karaoke.sh
+++ aws-karaoke.sh
@@ -20,11 +20,10 @@ done
 echo -e "You have chosen $SONG_CHOICE performed by by $SINGER_CHOICE\n"
 
 if [ -d "mp3s" ]; then
-    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >log.txt
-else
     mkdir mp3s
-    aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >log.txt
 fi
+
+aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >log.txt
 
 sleep 1
 
