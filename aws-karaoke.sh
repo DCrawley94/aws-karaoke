c1605a02e3e783520bbdab0834da73c9b6bf7a4d
DCrawley94 <duncan.crawley@gmail.com>
2023-07-11 14:35:29 +0100

    add new song and script update

--- aws-karaoke.sh
+++ aws-karaoke.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir mp3s 2>/dev/null
+
 songs=$(ls -1 songs | sed 's/\.[^.]*$//')
 singers="Aditi Amy Astrid Bianca Brian Camila Carla Carmen Celine Chantal Conchita Cristiano Dora Emma Enrique Ewa Filiz Gabrielle Geraint Giorgio Gwyneth Hans Ines Ivy Jacek Jan Joanna Joey Justin Karl Kendra Kevin Kimberly Lea Liv Lotte Lucia Lupe Mads Maja Marlene Mathieu Matthew Maxim Mia Miguel Mizuki Naja Nicole Olivia Penelope Raveena Ricardo Ruben Russell Salli Seoyeon Takumi Tatyana Vicki Vitoria Zeina Zhiyu Aria Ayanda Arlet Hannah Arthur Daniel Liam Pedro Kajal Hiujin Laura Elin Ida Suvi Ola Hala"
 
@@ -18,10 +20,6 @@ select singer in $singers; do
 done
 
 echo -e "You have chosen $SONG_CHOICE performed by by $SINGER_CHOICE\n"
-
-if [ -d "mp3s" ]; then
-    mkdir mp3s
-fi
 
 aws polly synthesize-speech --output-format mp3 --text "file://songs/$SONG_CHOICE.txt" --voice-id "$SINGER_CHOICE" "mp3s/$SINGER_CHOICE-$SONG_CHOICE.mp3" >log.txt
 
